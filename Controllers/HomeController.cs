a27de3fa253e2aca4a761ac919d8608036fcfc00
guhenriofc <you@example.com>
2021-02-02 16:38:38 -0200

    Perfil Atualizado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using EPlayers_AspNetCore.Models;
+using InstaDev_ProjetoGrupo3.Models;
 
-namespace EPlayers_AspNetCore.Controllers
+namespace InstaDev_ProjetoGrupo3.Controllers
 {
     public class HomeController : Controller
     {
