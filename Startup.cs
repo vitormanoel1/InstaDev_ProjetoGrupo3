458ea53dd818c845ce385a33a4a95d8e21933aa1
DEV <dev@info.local>
2021-02-02 16:55:00 -0300

    yuri atualização 3

--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace EPlayers_AspNetCore
+namespace InstaDev_ProjetoGrupo3
 {
     public class Startup
     {
