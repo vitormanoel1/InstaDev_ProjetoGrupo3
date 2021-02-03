87719ee7945f3856a9351f21ef1bea86e8f30a75
Caic Mateus <caicmateusfreitas@gmail.com>
2021-02-03 14:38:32 -0300

    erro cadastro solucionado

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -17,7 +17,7 @@ namespace InstaDev_ProjetoGrupo3.Controllers
             return View();
         }
 
-        [Route("Logar")]
+        [Route("Login")]
         public IActionResult Login(IFormCollection form)
         {
             // Lemos todos os arquivos do CSV
