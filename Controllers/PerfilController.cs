b0202ee2018b0a15385ffd731edffb1c591af74e
DEV <dev@info.local>
2021-02-04 16:08:08 -0200

    Perfil adicionado

--- Controllers/PerfilController.cs
+++ Controllers/PerfilController.cs
@@ -1,7 +1,14 @@
+using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 namespace InstaDev_ProjetoGrupo3.Controllers
 {
-    public class PerfilController
+        [Route("Perfil")]
+    public class PerfilController : Controller
     {
-        
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
